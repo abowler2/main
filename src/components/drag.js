c440ff9beee8c39e1e9236d90b9ca582789121c4
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-06-23 20:42:16 -0700

    linked swiper's css files to  drag

--- src/components/drag.js
+++ src/components/drag.js
@@ -1,30 +1,28 @@
 import Swiper from 'swiper';
 
-let mySwiper = new Swiper('.swiper-container', {
-  speed: 400,
-  spaceBetween: 100,
+
+let swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
 });
 
-
 function Drag() {
-  return(
+  return (
     <div className="swiper-container">
       <div className="swiper-wrapper">
-
         <div className="swiper-slide">Slide 1</div>
         <div className="swiper-slide">Slide 2</div>
         <div className="swiper-slide">Slide 3</div>
-
+        <div className="swiper-slide">Slide 4</div>
+        <div className="swiper-slide">Slide 5</div>
+        <div className="swiper-slide">Slide 6</div>
+        <div className="swiper-slide">Slide 7</div>
+        <div className="swiper-slide">Slide 8</div>
+        <div className="swiper-slide">Slide 9</div>
+        <div className="swiper-slide">Slide 10</div>
       </div>
-
-      <div className="swiper-pagination"></div>
-
-
-      <div className="swiper-button-prev"></div>
       <div className="swiper-button-next"></div>
-
-
-      <div className="swiper-scrollbar"></div>
+      <div className="swiper-button-prev"></div>
     </div>
   );
 };
