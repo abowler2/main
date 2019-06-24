26d4da9ba7c15efa92bb1232ac72bcd38101f1c9
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-06-23 22:26:25 -0700

    delted link and imported swiper css file

--- src/components/drag.js
+++ src/components/drag.js
@@ -1,28 +1,36 @@
 import Swiper from 'swiper';
-
+import '../../node_modules/swiper/dist/css/swiper.min.css';
 
 let swiper = new Swiper('.swiper-container', {
   slidesPerView: 3,
   spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
 function Drag() {
   return (
-    <div className="swiper-container">
-      <div className="swiper-wrapper">
-        <div className="swiper-slide">Slide 1</div>
-        <div className="swiper-slide">Slide 2</div>
-        <div className="swiper-slide">Slide 3</div>
-        <div className="swiper-slide">Slide 4</div>
-        <div className="swiper-slide">Slide 5</div>
-        <div className="swiper-slide">Slide 6</div>
-        <div className="swiper-slide">Slide 7</div>
-        <div className="swiper-slide">Slide 8</div>
-        <div className="swiper-slide">Slide 9</div>
-        <div className="swiper-slide">Slide 10</div>
+    <div className="grid-container">
+      <div className="column-24">
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">Slide 1</div>
+            <div className="swiper-slide">Slide 2</div>
+            <div className="swiper-slide">Slide 3</div>
+            <div className="swiper-slide">Slide 4</div>
+            <div className="swiper-slide">Slide 5</div>
+            <div className="swiper-slide">Slide 6</div>
+            <div className="swiper-slide">Slide 7</div>
+            <div className="swiper-slide">Slide 8</div>
+            <div className="swiper-slide">Slide 9</div>
+            <div className="swiper-slide">Slide 10</div>
+          </div>
+          <div className="swiper-button-next"></div>
+          <div className="swiper-button-prev"></div>         
+        </div>
       </div>
-      <div className="swiper-button-next"></div>
-      <div className="swiper-button-prev"></div>
     </div>
   );
 };
