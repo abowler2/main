d35ca93587f4130fc1ae77d1853153082be8bdea
dragcomponent <lvelazquez@elelwood.com>
2019-06-28 18:42:16 -0700

    playing with auto width

--- src/components/drag.js
+++ src/components/drag.js
@@ -17,16 +17,16 @@ function Drag() {
       <div className="column-24">
         <div className="swiper-container">
           <div className="swiper-wrapper">
-            <div className="swiper-slide">Slide 1</div>
-            <div className="swiper-slide">Slide 2</div>
-            <div className="swiper-slide">Slide 3</div>
-            <div className="swiper-slide">Slide 4</div>
-            <div className="swiper-slide">Slide 5</div>
-            <div className="swiper-slide">Slide 6</div>
-            <div className="swiper-slide">Slide 7</div>
-            <div className="swiper-slide">Slide 8</div>
-            <div className="swiper-slide">Slide 9</div>
-            <div className="swiper-slide">Slide 10</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/</div>
+            <div id="spred" className="swiper-slide">C/O https://placeholder.com/0</div>
           </div>
           <div className="swiper-button-next"><span className="swiper-button-prev-sm">&gt;</span> <span className="swiper-button-prev-lg">&gt;</span></div>
           <div className="circle">Drag</div>
